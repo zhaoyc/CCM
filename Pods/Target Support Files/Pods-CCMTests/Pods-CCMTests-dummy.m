83f548e2d2577c2f586ac1cd5f48dd9550755c1b
Zycle <zyc44@qq.com>
2017-05-04 17:52:28 +0800

    fisrst commit

---
+++ Pods/Target Support Files/Pods-CCMTests/Pods-CCMTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CCMTests : NSObject
+@end
+@implementation PodsDummy_Pods_CCMTests
+@end
---
+++ Pods/Target Support Files/Pods-CCMUITests/Pods-CCMUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CCMUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CCMUITests
+@end
